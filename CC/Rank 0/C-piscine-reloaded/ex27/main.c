5609be28a7dc32e962d064ca0217ff2f0806aa96
Alex Garcia Rodriguez <110727609+AlexGRDev@users.noreply.github.com>
2025-09-25 23:45:33 +0200

    feat: adding src folder an main function

---
+++ CC/Rank 0/C-piscine-reloaded/ex27/main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 15:04:45 by mvan-pee          #+#    #+#             */
+/*   Updated: 2025/09/25 23:41:18 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+void	display_file(int fd)
+{
+	char	buffer[1024];
+	int		bytes;
+
+	bytes = read(fd, buffer, sizeof(buffer));
+	while (bytes > 0)
+	{
+		write(1, buffer, bytes);
+		bytes = read(fd, buffer, sizeof(buffer));
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+
+	if (argc == 1)
+	{
+		write(2, "File name missing.\n", 19);
+		return (1);
+	}
+	if (argc > 2)
+	{
+		write(2, "Too many arguments.\n", 20);
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(2, "Cannot read file.\n", 18);
+		return (1);
+	}
+	display_file(fd);
+	close(fd);
+	return (0);
+}
