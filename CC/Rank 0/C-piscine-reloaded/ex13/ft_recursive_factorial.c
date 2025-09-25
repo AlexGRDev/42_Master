ae3cf9f6c92555c265b961d18e8d22c19b4d4743
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-25 21:46:01 +0200

    fix: ft_recursive_factorial function fixed

--- CC/Rank 0/C-piscine-reloaded/ex13/ft_recursive_factorial.c
+++ CC/Rank 0/C-piscine-reloaded/ex13/ft_recursive_factorial.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/24 11:58:48 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/24 12:01:49 by agarcia2         ###   ########.fr       */
+/*   Created: 2023/10/10 12:47:37 by mvan-pee          #+#    #+#             */
+/*   Updated: 2023/10/10 20:19:53 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_recursive_factorial(int nbr)
+int	ft_recursive_factorial(int nb)
 {
-	return (nbr * ft_recursive_factorial(nbr - 1));
+	if (nb < 0 || (nb > 12))
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
 }
