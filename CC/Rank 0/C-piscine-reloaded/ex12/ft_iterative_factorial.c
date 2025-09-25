aabb3b2ca2336821e416e9e088fe0a6b4e7fddbc
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-25 21:43:36 +0200

    fix: funcion ft_iterative factorial corrected

--- CC/Rank 0/C-piscine-reloaded/ex12/ft_iterative_factorial.c
+++ CC/Rank 0/C-piscine-reloaded/ex12/ft_iterative_factorial.c
@@ -3,21 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/24 11:48:59 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/24 11:53:15 by agarcia2         ###   ########.fr       */
+/*   Created: 2023/10/10 12:44:14 by mvan-pee          #+#    #+#             */
+/*   Updated: 2023/10/10 20:38:12 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nbr)
+int	ft_iterative_factorial(int nb)
 {
+	int	result;
 	int	i;
 
-	while (i < nbr)
-	{
-		nbr *= i;
-		i++;
-	}
-	return (i);
+	i = 1;
+	result = 1;
+	if ((nb < 0) || (nb > 12))
+		return (0);
+	if (nb == 1 || nb == 0)
+		return (1);
+	while (i <= nb)
+		result *= i++;
+	return (result);
 }
