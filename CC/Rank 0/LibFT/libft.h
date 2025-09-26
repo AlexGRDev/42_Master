7b6de9ba40ca9866ab35b4cda289a80d18a2e2ed
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 11:29:33 +0200

    feat: adding isprint to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 11:20:20 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/26 11:29:12 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define LIBFT_H
 
 int	ft_isascii(int c);
-int	ft_isalpha(int c);
 int	ft_isalnum(int c);
+int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isprint(int c);
+
 #endif
