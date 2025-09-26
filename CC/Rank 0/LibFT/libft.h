4bcc9bcc533e339b20c0b15404b7ec9a0489fe6b
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 11:21:01 +0200

    feat: adding function isascii to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,13 +6,14 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 11:14:03 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/26 11:20:20 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+int	ft_isascii(int c);
 int	ft_isalpha(int c);
 int	ft_isalnum(int c);
 int	ft_isdigit(int c);
