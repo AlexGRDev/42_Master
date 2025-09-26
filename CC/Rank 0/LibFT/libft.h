e71cfa985c5ee15895d5b42025a027649bf06135
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 13:32:45 +0200

    feat: adding mem function to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 13:12:58 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/26 13:31:30 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 
 # include <stddef.h>
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char *str);
+/* Checks */
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 
+/* Memory */
+void	ft_memset(void *b, int c, size_t len);
+
+/* Strings */
+int		ft_strlen(char *str);
 #endif
