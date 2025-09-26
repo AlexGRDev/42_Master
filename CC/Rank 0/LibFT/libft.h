66993fda438a857c8a0e295c775b87bad8d48cba
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 11:35:04 +0200

    feat: adding strlen to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,17 +6,18 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 11:29:12 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/26 11:34:41 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isascii(int c);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
+int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
+int	ft_strlen(int c);
 
 #endif
