6829ab5ef8cc68e81a986ec9906c961bbfa63150
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 17:21:44 +0200

    feat: adding bzero to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,13 +6,14 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 13:31:30 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/26 17:21:08 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <cstddef>
 # include <stddef.h>
 
 /* Checks */
@@ -23,6 +24,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 
 /* Memory */
+void	ft_bzero(void *s, size_t n);
 void	ft_memset(void *b, int c, size_t len);
 
 /* Strings */
