283bf452d1d9c47d44b33ec764fbc5795b4a6ca8
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-26 13:21:59 +0200

    feat: memory function finished

--- CC/Rank 0/LibFT/ft_memset.c
+++ CC/Rank 0/LibFT/ft_memset.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/26 13:12:58 by agarcia2         ###   ########.fr       */
+/*   Created: 2025/09/26 13:09:14 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/09/26 13:20:37 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char *str);
-
-#endif
+	str = (unsigned char *)b;
+	while (len > 0)
+	{
+		*(str)++ = (unsigned char)c;
+		len--;
+	}
+	return (b);
+}
