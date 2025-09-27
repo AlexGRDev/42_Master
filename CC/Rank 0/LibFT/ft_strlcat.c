f41c738fe0089523e2589cfcb9d414d011e1681a
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-27 11:07:27 +0200

    feat: function strlcat finshed

---
+++ CC/Rank 0/LibFT/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/27 10:02:23 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/09/27 11:04:57 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlcat(char *dst, char *src, size_t size)
+{
+	char	*d;
+	char	*s;
+	size_t	n;
+	size_t	dlen;
+
+	d = dst;
+	s = src;
+	n = size;
+	while (n-- != 0 && *d != '\0')
+		(*d)++;
+	dlen = d - dst;
+	n = size - dlen;
+	if (n == 0)
+		return (dlen + ft_strlen(s));
+	while (*s != '\0')
+	{
+		*(d)++ = *(s)++;
+		n--;
+	}
+	*d = '\0';
+	return (dlen + (s - src));
+}
