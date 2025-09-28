c74fccd38dedc9bc69729a3f0a45992d88cf77a2
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-28 09:16:43 +0200

    feat: adding strrchr to lib

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 11:04:17 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/28 08:42:19 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/28 09:16:10 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,5 @@ int		ft_strlen(char *str);
 int		ft_strlcpy(char *dst, char *src, size_t size);
 int		ft_strlcat(char *dst, char *src, size_t size);
 char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 #endif
