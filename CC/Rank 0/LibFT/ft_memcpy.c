f37c4dd385a9643027e1bd6d4111e50883870a8e
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-29 12:28:45 +0200

    feat: function memcpy finished

--- CC/Rank 0/LibFT/ft_memcpy.c
+++ CC/Rank 0/LibFT/ft_memcpy.c
@@ -1,30 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/28 11:35:03 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/29 12:28:09 by agarcia2         ###   ########.fr       */
+/*   Created: 2025/09/29 12:22:34 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/09/29 12:26:35 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
 
-/* Checks */
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-/* Memory */
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-/* Strings */
-size_t	ft_strlen(char *str);
-#endif
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	while (n > 0)
+	{
+		*s++ = *d++;
+		n--;
+	}
+	return (d);
+}
