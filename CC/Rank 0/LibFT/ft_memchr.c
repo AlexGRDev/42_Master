63a3efc1f4355a8006dd3c562d55125c2d1cf0dc
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-29 14:09:19 +0200

    fix(memchr): fix missing pointer increment and wrong return, ensure correct byte search

--- CC/Rank 0/LibFT/ft_memchr.c
+++ CC/Rank 0/LibFT/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:50:25 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/29 14:02:35 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/29 14:07:37 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	chr = (unsigned char)c;
 	while (n-- > 0)
 		if (*str == chr)
-			return ((void *)(*str + n));
+			return ((void *)str++);
 	return (NULL);
 }
