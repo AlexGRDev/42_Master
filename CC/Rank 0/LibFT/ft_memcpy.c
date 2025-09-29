3faf8e450c743de8f6d741068c446fedfb88b076
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-29 12:38:43 +0200

    fix(ft_memcpy): corregir dirección de copia, contador y retorno
    - Se corrige la dirección de copia (*d = *s en vez de *s = *d).
    - Se arregla el contador (n-- en vez de n++) evitando bucle infinito.
    - Se retorna el puntero original dst en vez del puntero ya avanzado.
    - Se añade protección para el caso (dst == NULL && src == NULL).

--- CC/Rank 0/LibFT/ft_memcpy.c
+++ CC/Rank 0/LibFT/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:22:34 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/09/29 12:26:35 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/09/29 12:36:34 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
-	while (n > 0)
-	{
-		*s++ = *d++;
-		n--;
-	}
-	return (d);
+	if (!dst && !src)
+		return (NULL);
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dst);
 }
