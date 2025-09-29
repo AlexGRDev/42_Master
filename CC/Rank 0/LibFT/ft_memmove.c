a2790e403d7a8012b1eab898ddc92c2fb2aa22e9
agarcia2 <agarcia2@student.42barcelona.com>
2025-09-29 12:59:45 +0200

    feta(ft_memmvoe): function finished

---
+++ CC/Rank 0/LibFT/ft_memmove.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 12:46:52 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/09/29 12:49:53 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*s;
+	unsigned char	*d;
+
+	s = (unsigned char *)src;
+	d = (unsigned char *)dst;
+	while (len-- > 0)
+		*s++ = *d++;
+	return (dst);
+}
