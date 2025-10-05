db78c77e622c073f010f759ac719aa536b01616d
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-05 12:57:46 +0200

    feat(stdup): function finished

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 11:35:03 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/04 18:16:54 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/04 19:42:56 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s1);
 #endif
