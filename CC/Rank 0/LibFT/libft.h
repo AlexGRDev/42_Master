3320e07a189e7188a51c961eeffae86525e02797
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-09 15:47:18 +0200

    style(libft): reorganize header by category, type and alphabetical order

--- CC/Rank 0/LibFT/libft.h
+++ CC/Rank 0/LibFT/libft.h
@@ -5,45 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/28 11:35:03 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/09 11:52:19 by agarcia2         ###   ########.fr       */
+/*   Created: 2025/10/09 15:44:51 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/10/09 15:45:38 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
 
-/* Checks */
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
+/* → funciones que devuelven int y analizan caracteres */
 int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
 int		ft_isprint(int c);
-int		ft_toupper(int c);
 int		ft_tolower(int c);
-/* Memory */
-int		ft_memcmp(void *s1, void *s2, size_t n);
-void	*ft_memset(void *b, int c, size_t len);
+int		ft_toupper(int c);
+
+/* → funciones que devuelven punteros o gestionan bloques de memoria */
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-/* Strings */
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
+void	*ft_memset(void *b, int c, size_t len);
+
+/* → funciones que devuelven size_t (longitudes) */
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+/* → funciones que devuelven int (comparaciones) */
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/* → funciones que devuelven punteros a char */
 char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s1);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strtrim(const char *s1, const char *set);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+/* → funciones que devuelven arrays de strings */
 char	**ft_split(char const *s, char c);
+
+/* → funciones que convierten entre tipos numéricos y cadenas */
+int		ft_atoi(const char *str);
+
 #endif
