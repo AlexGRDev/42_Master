95a7f81636f5422ac2466b839fcde299b2d49997
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-19 22:08:07 +0200

    fix(header): change name

--- Rank 1/ft_printf/ft_printf.h
+++ Rank 1/ft_printf/ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 20:02:53 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/19 20:50:26 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/19 20:57:30 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,5 @@
 
 # include <stdarg.h>
 # include "LibFT/libft.h"
-
-int	ft_printf(const char *format, ...);
-int	ft_formats(va_list args, const char format);
-int	ft_putchar_pf(char c);
-int	ft_putstr_pf(char *s);
-int	ft_putnbr_pf(int n);
-int	ft_put_unsigned_pf(unsigned int n);
-int	ft_puthex_pf(unsigned int n, const char format);
-int	ft_putptr_pf(unsigned long long ptr);
 
 #endif
