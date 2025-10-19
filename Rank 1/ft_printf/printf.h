b914c1580b8a09eaadb9ed413ce9c0ec94599273
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-19 20:51:09 +0200

    feat(header): adding all the functiosn that i will use

--- Rank 1/ft_printf/printf.h
+++ Rank 1/ft_printf/printf.h
@@ -6,11 +6,23 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 20:02:53 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/19 20:24:06 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/19 20:50:26 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "LibFT/libft.h"
+
+int	ft_printf(const char *format, ...);
+int	ft_formats(va_list args, const char format);
+int	ft_putchar_pf(char c);
+int	ft_putstr_pf(char *s);
+int	ft_putnbr_pf(int n);
+int	ft_put_unsigned_pf(unsigned int n);
+int	ft_puthex_pf(unsigned int n, const char format);
+int	ft_putptr_pf(unsigned long long ptr);
 
 #endif
