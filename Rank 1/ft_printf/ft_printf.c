d7ddbbab7b32cf57af25f0639e35fcab165d839c
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-20 10:28:45 +0200

    feat: add ft_printf.c

---
+++ Rank 1/ft_printf/ft_printf.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 22:12:38 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/10/20 10:23:41 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		len;
+
+	len = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			len += ft_formats(args, format);
+		}
+		else
+			len += write(1, format, 1);
+		format++;
+	}
+	va_end(args);
+	return (len);
+}
