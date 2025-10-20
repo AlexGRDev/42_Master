c8b419469b6ef3f4220e56ff629f399a7dc8e2c9
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-20 10:30:38 +0200

    fix: update header ft_printf.h

--- Rank 1/ft_printf/ft_printf.h
+++ Rank 1/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 20:02:53 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/19 20:57:30 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/20 10:14:53 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "LibFT/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 
+int	ft_printf(const char *format, ...);
+int	ft_print_char(int c);
+int	ft_print_str(char *s);
+int	ft_print_nbr(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_hex(unsigned int n, const char format);
+int	ft_print_ptr(unsigned long long ptr);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr_base(unsigned long long n, char *base);
+int	ft_formats(va_list args, const char *format);
 #endif
