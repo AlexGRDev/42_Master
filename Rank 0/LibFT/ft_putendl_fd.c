e09248e8dec4ff930310fb597ad35861503bf48f
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-22 10:37:39 +0200

    fix(put_endl_fd): deleting the insesary loop

--- Rank 0/LibFT/ft_putendl_fd.c
+++ Rank 0/LibFT/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 07:44:50 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/17 08:08:28 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/21 13:56:26 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	while (*s)
-		write(fd, s++, 1);
+	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
