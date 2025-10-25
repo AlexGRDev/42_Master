a6d688858368fae1f9fb0ec1d993f54e653f097e
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-25 22:20:30 +0200

    norm: Error: EMPTY_LINE_FUNCTION  (line:  24, col:   1):	Empty line in function Error: NEWLINE_PRECEDES_FUNC (line:  26, col:   1):	Functions must be separated by a newline Error: TOO_MANY_TAB         (line:  26, col:   1):	Extra tabs for indent level Error: SPACE_REPLACE_TAB    (line:  29, col:  12):	Found space when expecting tab Error: MISALIGNED_VAR_DECL  (line:  29, col:  13):	Misaligned variable declaration Error: SPACE_EMPTY_LINE     (line:  30, col:   1):	Space on empty line Error: SPACE_AFTER_KW       (line:  33, col:   5):	Missing space after keyword Error: SPC_BFR_PAR          (line:  33, col:  10):	Missing space before parenthesis (brace/bracket) Error: EMPTY_LINE_FUNCTION  (line:  42, col:   1):	Empty line in function

--- Rank 1/ft_printf/ft_printf.c
+++ Rank 1/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 18:05:19 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/25 18:22:11 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/25 22:18:53 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ static int	formats(va_list args, const char str)
 
 	count = 0;
 	if (str == 'c')
-		count += ft_putchar(va_arg(args, int);
-	va_end(args);
+		count += ft_putchar(va_arg(args, int));
+	va_end (args);
 	return (count);
-
 }
-	int	ft_printf(const char *str, ...)
+
+int	ft_printf(const char *str, ...)
 {
-	int	count;
-	va_list args;
-	
-	va_start(args str);
+	int		count;
+	va_list	args;	
+
+	va_start(args, str);
 	count = 0;
-	while(*str)
+	while (*str)
 	{
 		if (*str == '%')
 		{
@@ -39,7 +39,6 @@ static int	formats(va_list args, const char str)
 		}
 		else
 			count += ft_putchar(*str);
-
 		count++;
 	}
 	va_end(args);
