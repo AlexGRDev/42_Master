e7efa80c1389ce7ba166d6c3a4155a19bae84c25
agarcia2 <agarcia2@student.42barcelona.com>
2025-10-26 16:50:46 +0100

    ns

--- Rank 1/ft_printf/ft_printf.c
+++ Rank 1/ft_printf/ft_printf.c
@@ -6,20 +6,20 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 18:05:19 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/25 22:18:53 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/26 16:30:04 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	formats(va_list args, const char str)
+static int	formats(va_list args, const char **str)
 {
 	int	count;
 
 	count = 0;
-	if (str == 'c')
+	if (**str == 'c')
 		count += ft_putchar(va_arg(args, int));
-	va_end (args);
+	va_end(args);
 	return (count);
 }
 
@@ -34,13 +34,14 @@ int	ft_printf(const char *str, ...)
 	{
 		if (*str == '%')
 		{
-			count += formats(args, *str);
-			count++;
+			str++;
+			count += formats(args, &str);
 		}
 		else
 			count += ft_putchar(*str);
-		count++;
+		str++;
 	}
+	printf("%i", count);
 	va_end(args);
 	return (count);
 }
--- Rank 1/ft_printf/ft_printf.h
+++ Rank 1/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 14:10:24 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/25 22:14:16 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/10/26 10:58:01 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include <stdio.h>
 
 int	ft_printf(const char *str, ...);
 int	ft_putchar(int c);
