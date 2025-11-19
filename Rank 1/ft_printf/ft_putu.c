f53ce7303bc13b23f39f11b3e9beedccfd6a503c
agarcia2 <agarcia2@student.42barcelona.com>
2025-11-19 09:58:34 +0100

    Fix recursive printing in ft_putu

--- Rank 1/ft_printf/ft_putu.c
+++ Rank 1/ft_printf/ft_putu.c
@@ -6,23 +6,20 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 15:16:48 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/11/11 15:29:12 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/11/17 12:46:33 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <wchar.h>
 
 int	ft_putu(unsigned int n)
 {
-	unsigned int	ptr;
-	
+	int	ptr;
+
 	ptr = 0;
-	if(n > 9)
-	{
-		ptr += ft_putchar(n / 10);
-		ptr += ft_putchar(n % 10 + '0');
-	}
-	else
-		ptr = ft_putchar(n + '0');
+	if (n > 9)
+		ptr += ft_putu(n / 10);
+	ptr += ft_putchar((n % 10) + '0');
 	return (ptr);
 }
