9ea21373dc26c5c844f5c3fe1feae51debc39bab
agarcia2 <agarcia2@student.42barcelona.com>
2025-11-19 09:58:34 +0100

    Adjust NULL string handling in ft_putstr

--- Rank 1/ft_printf/ft_putstr.c
+++ Rank 1/ft_printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 18:54:42 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/11/03 12:27:37 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/11/16 22:16:07 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_putstr(char *str)
 {
 	if (!str)
-		str = "(null)";
+		return (write(1, "(null)", 6));
 	return (write(1, str, ft_strlen(str)));
 }
