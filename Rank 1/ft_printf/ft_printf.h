516b9aaef65cc31eef6865277db274554ad88d07
agarcia2 <agarcia2@student.42barcelona.com>
2025-11-25 10:32:30 +0100

    Add stdint include and tidy printf header

--- Rank 1/ft_printf/ft_printf.h
+++ Rank 1/ft_printf/ft_printf.h
@@ -6,17 +6,14 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 14:10:24 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/11/17 12:45:29 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/11/25 10:28:36 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include <stdio.h>
 # include "./LibFT/libft.h"
+# include <stdint.h>
 
 int	ft_printf(const char *str, ...);
 int	ft_putchar(int c);
