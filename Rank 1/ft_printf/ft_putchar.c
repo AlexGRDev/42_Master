5b7a3083ce5164da8c3c5058ee171f596be7518e
agarcia2 <agarcia2@student.42barcelona.com>
2025-11-25 10:32:44 +0100

    Ensure robust write handling in ft_putchar

--- Rank 1/ft_printf/ft_putchar.c
+++ Rank 1/ft_printf/ft_putchar.c
@@ -6,11 +6,12 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 11:00:00 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/11/24 11:23:21 by agarcia2         ###   ########.fr       */
+/*   Updated: 2025/11/25 10:21:03 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <unistd.h>
 
 int	ft_putchar(int c)
 {
