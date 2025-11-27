b0c71014b2f322685a0aa15719d5e3ef1ff19193
Alex Garcia Rodriguez <110727609+AlexGRDev@users.noreply.github.com>
2025-11-27 19:01:30 +0100

    pipex

---
+++ Rank 2/pipex/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 16:10:00 by agarcia2          #+#    #+#             */
+/*   Updated: 2025/11/27 17:00:42 by agarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 5)
+	{
+		ft_printf("Usage: %s infile cmd1 cmd2 outfile\n", argv[0]);
+		return (1);
+	}
+	return (pipex(&argv[1], &envp[0]));
+}
