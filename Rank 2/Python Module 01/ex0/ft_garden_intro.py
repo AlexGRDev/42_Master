9779d6d4752f4eb667fe7095b1f2869faae1aca9
agarcia2 <agarcia2@student.42barcelona.com>
2026-01-18 10:57:11 +0100

    M01 adding ex01  finished

---
+++ Rank 2/Python Module 01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+def ft_garden_intro():
+    plant_name = "Sunflower"
+    plant_height = 120
+    plant_age = 45
+
+    print("Plant name:", plant_name)
+    print("Height:", plant_height, "cm")
+    print("Age:", plant_age, "days")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
