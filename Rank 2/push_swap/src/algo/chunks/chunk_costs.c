e3e33cd05e8966664546e940c0d6bf7dd998b283
agarcia2 <agarcia2@student.42barcelona.com>
2026-01-28 14:52:02 +0100

    norm: Eerror: line 53 too long 25 lines

--- Rank 2/push_swap/src/algo/chunks/chunk_costs.c
+++ Rank 2/push_swap/src/algo/chunks/chunk_costs.c
@@ -6,58 +6,63 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 08:59:48 by agarcia2          #+#    #+#             */
-/*   Updated: 2026/01/26 11:16:24 by agarcia2         ###   ########.fr       */
+/*   Updated: 2026/01/28 14:50:52 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int abs_val(int n)
+static int	abs_val(int n)
 {
 	if (n < 0)
 		return (-n);
 	return (n);
 }
 
-void chunk_calc_costs(t_chunk *c, t_stack *s)
+static int	calc_rot_cost(int index, int len)
 {
-	int i;
-	int a;
-	int b;
+	if (index <= len / 2)
+		return (index);
+	else
+		return (index - len);
+}
+
+static int	calc_final_cost(int cost_a, int cost_b)
+{
+	if ((cost_a > 0 && cost_b > 0) || (cost_a < 0 && cost_b < 0))
+	{
+		if (abs_val(cost_a) > abs_val(cost_b))
+			return (abs_val(cost_a));
+		else
+			return (abs_val(cost_b));
+	}
+	else
+		return (abs_val(cost_a) + abs_val(cost_b));
+}
+
+void	chunk_calc_costs(t_chunk *c, t_stack *s)
+{
+	int	i;
+	int	a;
+	int	b;
 
 	i = 0;
 	while (i < s->len_b)
 	{
 		a = c->target[i];
-		if (a <= s->len_a / 2)
-			c->cost_a[i] = a;
-		else
-			c->cost_a[i] = a - s->len_a;
-
 		b = c->pos[i];
-		if (b <= s->len_b / 2)
-			c->cost_b[i] = b;
-		else
-			c->cost_b[i] = b - s->len_b;
-
-		if ((c->cost_a[i] > 0 && c->cost_b[i] > 0) || (c->cost_a[i] < 0 && c->cost_b[i] < 0))
-		{
-			if (abs_val(c->cost_a[i]) > abs_val(c->cost_b[i]))
-				c->cost[i] = abs_val(c->cost_a[i]);
-			else
-				c->cost[i] = abs_val(c->cost_b[i]);
-		}
-		else
-			c->cost[i] = abs_val(c->cost_a[i]) + abs_val(c->cost_b[i]);
+		c->cost_a[i] = calc_rot_cost(a, s->len_a);
+		c->cost_b[i] = calc_rot_cost(b, s->len_b);
+		c->cost[i] = calc_final_cost(c->cost_a[i], c->cost_b[i]);
 		i++;
 	}
 }
 
-int find_cheapest(t_chunk *c, t_stack *s)
+int	find_cheapest(t_chunk *c, t_stack *s)
 {
-	int i;
-	int best;
-	int id;
+	int	i;
+	int	best;
+	int	id;
 
 	i = 0;
 	id = 0;
