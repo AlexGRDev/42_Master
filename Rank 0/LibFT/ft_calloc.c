21b07f504db24aa3b973ad5bc62cf758b0bf070b
agarcia2 <agarcia2@student.42barcelona.com>
2026-03-24 10:05:04 +0100

    fix: fixing malloc multiplication in calloc functions

--- Rank 0/LibFT/ft_calloc.c
+++ Rank 0/LibFT/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: agarcia2 <agarcia2@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 18:09:54 by agarcia2          #+#    #+#             */
-/*   Updated: 2025/10/22 10:34:03 by agarcia2         ###   ########.fr       */
+/*   Updated: 2026/03/24 10:03:28 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	int		len;
 	void	*ptr;
 
 	if (size != 0 && count > (size_t)-1 / size)
 		return (NULL);
-	ptr = malloc(count * size);
+	len = count * size;
+	ptr = malloc(len);
 	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, count * size);
