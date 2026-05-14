09f6ba37cda0ffef1a2a0a6271ea73d9a43af952
agarcia2 <agarcia2@student.42barcelona.com>
2026-05-14 11:17:37 +0200

    add: adding 42header

--- Rank 2/Python/PY00/ex7/ft_seed_inventory.py
+++ Rank 2/Python/PY00/ex7/ft_seed_inventory.py
@@ -1,3 +1,14 @@
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    ft_seed_inventory.py                              :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: agarcia2 <agarcia2@student.42barcelona.c  +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/05/14 11:15:50 by agarcia2         #+#    #+#              #
+#    Updated: 2026/05/14 11:15:52 by agarcia2        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == 'packets':
         print(f'{seed_type.capitalize()} seeds: {quantity} {unit} total')
