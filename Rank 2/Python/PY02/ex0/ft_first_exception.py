97f2aa16a836d60d36491759416a94fd1aeb065b
agarcia2 <agarcia2@student.42barcelona.com>
2026-05-14 13:38:33 +0200

    PY02 ex1 && ex2

--- Rank 2/Python/PY02/ex0/ft_first_exception.py
+++ Rank 2/Python/PY02/ex0/ft_first_exception.py
@@ -1,10 +1,32 @@
-#!/usr/bin/python3
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    ft_first_exception.py                             :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: agarcia2 <agarcia2@student.42barcelona.c  +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/05/14 11:55:42 by agarcia2         #+#    #+#              #
+#    Updated: 2026/05/14 12:11:03 by agarcia2        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+
+# !/usr/bin/python3
+
+from typing import Any
+
 
 def input_temperature(temp_str: str) -> int:
     tmp: int
 
     tmp = int(temp_str)
     return (tmp)
+
+
+def test_temp(tmp: Any) -> None:
+    try:
+        print(f"Temperature is now {input_temperature(tmp)}°C")
+    except Exception as e:
+        print(f"Caught input_temperature error: {e}")
 
 
 def main() -> int:
@@ -14,17 +36,11 @@ def main() -> int:
     print("=== Garden Temperature ===")
     print()
     print(f"Input data is '{tmp}'")
-    try:
-        print(f"Temperature is now {input_temperature(tmp)}°C")
-    except Exception as e:
-        print(f"Caught input_temperature error: {e}")
+    test_temp(tmp)
     print()
     tmp = 'abc'
     print(f"Input data is '{tmp}'")
-    try:
-        print(f"Temperature is now {input_temperature(tmp)}°C")
-    except Exception as e:
-        print(f"Caught input_temperature error: {e}")
+    test_temp(tmp)
     print()
     print("All tests completed - program didn't crash!")
     return (0)
--- Rank 2/Python/PY02/ex1/ft_raise_exception.py
+++ Rank 2/Python/PY02/ex1/ft_raise_exception.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    ft_raise_exception.py                             :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: agarcia2 <agarcia2@student.42barcelona.c  +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/05/14 12:12:24 by agarcia2         #+#    #+#              #
+#    Updated: 2026/05/14 12:43:44 by agarcia2        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+
+# !/usr/bin/python3
+
+from ex0 import ft_first_exception
+
+
+def ft_raise_exception(tmp: int) -> int:
+    if (tmp > 40):
+        raise ValueError(f"{tmp}°C is too hot for plants (max 40°C)")
+    if (tmp < 0):
+        raise ValueError(f"{tmp}°C is too hot for plants (max 40°C)")
+    return (tmp)
+
+
+def main() -> int
+    return (0)
