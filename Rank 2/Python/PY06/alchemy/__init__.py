694f9865e5ad3647e33bcdbcce5a6c812d7b328f
Alex Garcia Rodriguez <agarcia2@cbr1s5.42barcelona.com>
2026-07-10 10:24:00 +0200

    fitx(__init__.py): addinf eathr creation in package file

--- Rank 2/Python/PY06/alchemy/__init__.py
+++ Rank 2/Python/PY06/alchemy/__init__.py
@@ -6,17 +6,18 @@
 #    By: agarcia2 <agarcia2@student.42barcelona.c  +#+  +:+       +#+         #
 #                                                +#+#+#+#+#+   +#+            #
 #    Created: 2026/06/15 19:01:05 by agarcia2         #+#    #+#              #
-#    Updated: 2026/06/15 19:01:39 by agarcia2        ###   ########.fr        #
+#    Updated: 2026/07/10 10:17:22 by agarcia2        ###   ########.fr        #
 #                                                                             #
 # *************************************************************************** #
 
 
-from alchemy.elements import create_air
+from alchemy.elements import create_air, create_earth
 from alchemy.potions import strength_potion
 from alchemy.transmutation import lead_to_gold
 
 __all__: list[str] = [
     "create_air",
+    "create_earth",
     "strength_potion",
     "lead_to_gold",
     "heal",
