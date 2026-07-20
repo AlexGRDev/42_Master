db2c9693d614d7c1c0059c0b1d89146c63af3569
Alex Garcia Rodriguez <agarcia2@car15s2.42barcelona.com>
2026-07-20 15:22:51 +0200

    refactor(main): delegate to tests/run_all_tests, keep main under norm line limit

--- Rank 1/ft_printf/main.c
+++ Rank 1/ft_printf/main.c
@@ -6,25 +6,23 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 12:28:00 by agarcia2          #+#    #+#             */
-/*   Updated: 2026/07/20 12:45:40 by agarcia2         ###   ########.fr       */
+/*   Updated: 2026/07/20 15:30:00 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "tests/test_runners.h"
 
-int	main(int ac, char **av)
+#define TOTAL_TESTS 18
+
+int	main(void)
 {
-	char	*c;
-	int		ft;
-	int		og;
-	(void)ac;
+	int	passed;
 
-	c = av[1];
-	ft = ft_printf("ft: %s\n", c);
-	og = printf("og: %s\n", c);
-	if (ft == og)
-		printf("OK (ft=%d og=%d)\n", ft, og);
-	else
-		printf("KO (ft=%d og=%d)\n", ft, og);
+	setbuf(stdout, NULL);
+	passed = run_all_tests();
+	printf("\n%d/%d tests OK\n", passed, TOTAL_TESTS);
+	if (passed != TOTAL_TESTS)
+		return (1);
 	return (0);
 }
