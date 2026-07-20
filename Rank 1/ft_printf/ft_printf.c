306839229f1b432af939199bc864622c1600a7c0
Alex Garcia Rodriguez <agarcia2@car15s2.42barcelona.com>
2026-07-20 15:22:36 +0200

    feat(ft_printf): wire %d/%i/%p into the t_fmt dispatch table

--- Rank 1/ft_printf/ft_printf.c
+++ Rank 1/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: agarcia2 <agarcia2@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 11:30:48 by agarcia2          #+#    #+#             */
-/*   Updated: 2026/07/20 13:09:46 by agarcia2         ###   ########.fr       */
+/*   Updated: 2026/07/20 15:11:15 by agarcia2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,7 @@ static int	format(va_list *args, const char *str, t_fmt *formats)
 	while (formats->fmt)
 	{
 		if (*(formats->fmt) == *str)
-		{
-			if (*str == 'c')
-				return (formats->c(args));
-			else if (*str == 's')
-				return (formats->s(args));
-		}
+			return (formats->f(args));
 		formats++;
 	}
 	return (0);
@@ -32,7 +27,7 @@ static int	print_f(const char **fmt, va_list *args, t_fmt *formats)
 {
 	int	count;
 
-	if (**fmt == '%' && *(fmt + 1))
+	if (**fmt == '%' && *(*fmt + 1))
 	{
 		(*fmt)++;
 		count = format(args, *fmt, formats);
@@ -50,23 +45,22 @@ int	ft_printf(const char *fmt, ...)
 	int			count;
 	int			printed;
 	const char	*ptr;
-	t_fmt		formats[3];
+	t_fmt		formats[6];
 
-	formats[0] = (t_fmt){"c", ft_putchar, NULL};
-	formats[1] = (t_fmt){"s", NULL, ft_putstr};
-	formats[2] = (t_fmt){NULL, NULL, NULL};
+	formats[0] = (t_fmt){"c", ft_putchar};
+	formats[1] = (t_fmt){"s", ft_putstr};
+	formats[2] = (t_fmt){"i", ft_putnbr};
+	formats[3] = (t_fmt){"d", ft_putnbr};
+	formats[4] = (t_fmt){"p", ft_putptr};
+	formats[5] = (t_fmt){0, NULL};
 	va_start(args, fmt);
 	ptr = fmt;
 	count = 0;
-	printed = 0;
 	while (*ptr)
 	{
 		printed = print_f(&ptr, &args, formats);
 		if (printed == -1)
-		{
-			va_end(args);
-			return (-1);
-		}
+			return (va_end(args), -1);
 		count += printed;
 	}
 	va_end(args);
